9b4f58cdb38fbb349bfccb74895f0d6ad6ce9ac7
yann-papouin <yann.papouin@gmail.com>
2021-01-08 19:43:07 +0100

    Sample update bat file

---
+++ update.bat
@@ -0,0 +1,40 @@
+﻿REM Set codepage to UTF-8
+chcp 65001 > nul
+
+call .venv/Scripts/activate.bat
+
+python generate_categories.py ^
+    --base-path=E:\Animations ^
+    --cleanlist-path=user_clean_list.py ^
+    --ignorelist-path=user_ignore_list.py ^
+    --remove-dir ^
+    --use-subcategories
+    
+python generate_categories.py ^
+    --base-path=E:\Emissions ^
+    --cleanlist-path=user_clean_list.py ^
+    --ignorelist-path=user_ignore_list.py ^
+    --remove-dir ^
+    --use-subcategories
+    
+python generate_categories.py ^
+    --base-path=E:\Films ^
+    --cleanlist-path=user_clean_list.py ^
+    --ignorelist-path=user_ignore_list.py ^
+    --remove-dir ^
+    --use-subcategories
+
+python generate_categories.py ^
+    --base-path=E:\Séries ^
+    --cleanlist-path=user_clean_list.py ^
+    --ignorelist-path=user_ignore_list.py ^
+    --remove-dir ^
+    --use-subcategories
+    
+python generate_categories.py ^
+    --base-path=E:\Spectacles ^
+    --cleanlist-path=user_clean_list.py ^
+    --ignorelist-path=user_ignore_list.py ^
+    --remove-dir ^
+    --use-subcategories
+
